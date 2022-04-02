f95b1a8c1830ca9b6a57be15a72979c2aef84b15
Clark0 <caoliu98@yahoo.com>
2022-04-02 21:12:30 +0800

    status bar on all windows

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
-#define TERM "st"
+#define TERM "alacritty"
 #define BROWSER "firefox"
 
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
